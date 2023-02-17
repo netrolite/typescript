1ada99f9e2b08b7b5339d46bb8572ce557eba127
Matvey <morozovmatvej75@gmail.com>
2023-02-17 22:14:51 +0300

    nullish coalescing & optional properties and methods

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,38 @@
-function sum(a: number, b: number, c: number = 5) {
-  return a + b + c;
+interface Vehicle {
+  type: "car" | "boat" | "plane",
+  getType?: Function,
+  license?: {
+    madeAt: Date,
+    expiresAt: Date,
+    getLicense?: Function
+  }
 }
 
-console.log(sum(1, 2, 13));
+interface Person {
+  name?: string | number,
+  vehicles: Vehicle[]
+}
+
+const bobsCar: Vehicle = {
+  type: "plane",
+  getType() {
+    return this.type;
+  },
+  license: {
+    getLicense() {
+      return this.expiresAt;
+    },
+    madeAt: new Date("Feb 19, 1992"),
+    expiresAt: new Date("Feb 18, 1992")
+  }
+}
+
+const bob: Person = {
+  name: "",
+  vehicles: [bobsCar]
+}
+
+const expiresAt = bob.vehicles[0]?.license?.getLicense?.() ?? "unknown";
+const type = bob.vehicles[0]?.getType?.() ?? "type is not defined";
+console.log(expiresAt);
+console.log(type);
