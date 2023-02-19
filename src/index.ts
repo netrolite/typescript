d135e7a74caaaa3e1a1f9e26f4fb3de10addca79
Matvey <morozovmatvej75@gmail.com>
2023-02-19 12:43:50 +0300

    type assertions testing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
-type One = string;
-type Two = "you";
-type Three = "hello";
+interface Person {
+  name: string,
+  job: string
+}
 
-// typescript knows about the type but doesn't perform any checks
-// so the following does not throw any errors:
-const a: One = "hi";
-const b = a as Two;
-const c = <Three>a;
+interface Animal {
+  name: string
+}
+
+// compiler doens't care!!!
+const bob = {
+  name: "Bob",
+  job: "web developer"
+} as Animal;
+
+// doesn't care either!
+const rob: Animal = {
+
+} as Animal;
+
+/*
+  // error: property "job" is missing in type "Animal" but is required in type "Person"
+  const mary: Person = {
+
+  } as Animal;
+*/
+
+// no errors
+const john: Person = {
+  name: "John"
+} as Person
+
+console.log(bob);
