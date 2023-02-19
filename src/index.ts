00198e72c6d7b589aff407b4c8f8b78c6dd3e723
Matvey <morozovmatvej75@gmail.com>
2023-02-19 13:09:19 +0300

    non-null assertion

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-const selectedByTag = document.querySelector("img")!;
-const selectedById = document.querySelector("#image") as HTMLImageElement;
-
+// infers that the type is HTMLImageElement but adding an exclamation mark to denote that I know what I'm doing so it's definitely not null
+const selectedByTagName = document.querySelector("img")!;
+// infers the type as "Element" (not even "HTMLElement") so adding type assertion to make the compiler think it's type of HTMLImageElement and is not null
+const selectedById = document.querySelector("#img") as HTMLImageElement;
