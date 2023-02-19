11280b77619222105d9964c20eb44c9a4b6863fc
Matvey <morozovmatvej75@gmail.com>
2023-02-19 13:05:02 +0300

    add tsconfig (deleted and forgot to add back)

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,3 @@
-interface Person {
-  name: string,
-  job: string
-}
+const selectedByTag = document.querySelector("img")!;
+const selectedById = document.querySelector("#image") as HTMLImageElement;
 
-interface Animal {
-  name: string
-}
-
-// compiler doens't care!!!
-const bob = {
-  name: "Bob",
-  job: "web developer"
-} as Animal;
-
-// doesn't care either!
-const rob: Animal = {
-
-} as Animal;
-
-/*
-  // error: property "job" is missing in type "Animal" but is required in type "Person"
-  const mary: Person = {
-
-  } as Animal;
-*/
-
-// no errors
-const john: Person = {
-  name: "John"
-} as Person
-
-console.log(bob);
