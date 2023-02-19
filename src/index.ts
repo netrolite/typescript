fbe7adff0827d28047b79124afb147f4bff55742
Matvey <morozovmatvej75@gmail.com>
2023-02-19 10:51:37 +0300

    type assertions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+const name: any = "hello";
 
+const lengthWithAs = (name as string).length;
+const lengthWithAngleBrackets = (<string>name).length;
+console.log(lengthWithAs);
+console.log(lengthWithAngleBrackets);
+console.log(lengthWithAs === lengthWithAngleBrackets);
