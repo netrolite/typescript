6a10e196a7bfcd0b2352d37e9036af0a430f8087
Matvey <morozovmatvej75@gmail.com>
2023-02-19 13:46:36 +0300

    modifying the DOM with the help of type asssertions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-// infers that the type is HTMLImageElement but adding an exclamation mark to denote that I know what I'm doing so it's definitely not null
-const selectedByTagName = document.querySelector("img")!;
-// infers the type as "Element" (not even "HTMLElement") so adding type assertion to make the compiler think it's type of HTMLImageElement and is not null
-const selectedById = document.querySelector("#img") as HTMLImageElement;
+const yearNode = document.querySelector("#year") as HTMLTimeElement;
+const year = new Date().getFullYear().toString();
+
+yearNode.setAttribute("datetime", year);
+yearNode.textContent = year;
