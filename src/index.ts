4d9c87ae4ec6a9cdda7578c6c7f44690a01511b3
Matvey <morozovmatvej75@gmail.com>
2023-02-19 10:08:45 +0300

    clean up compiler options

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-// custom type guard
-function isArray(val: any): boolean {
-  return Array.isArray(val);
-}
 
-function checkToSeeIfValIsArray(val: any): string {
-  // using a custom type guard
-  if (isArray(val)) return "yes, it is indeed an array";
-  return "no, it is not an array";
-}
-
-console.log(checkToSeeIfValIsArray(3));
