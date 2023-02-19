6ecabff3af4dcc76e1a7615131c6c3d8e871d51e
Matvey <morozovmatvej75@gmail.com>
2023-02-19 14:46:53 +0300

    classes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,42 @@
-const yearNode = document.querySelector("#year") as HTMLTimeElement;
-const year = new Date().getFullYear().toString();
+class Person {
+  secondLang!: string;
 
-yearNode.setAttribute("datetime", year);
-yearNode.textContent = year;
+  // private can only be accessed from this class
+  // protected can be accessed from derived (extended) classes
+  constructor(
+    private readonly name: string,
+    private musicPref: string,
+    private age: number,
+    private lang: string = "Typescript"
+  ) {
+    this.name = name;
+    this.musicPref = musicPref;
+    this.age = age;
+    this.lang = lang;
+  }
+
+  public getName = () => this.name;
+
+  public getMusicPref = () => this.musicPref;
+  public setMusicPref = (musicPref: string) => this.musicPref = musicPref;
+
+  public getAge = () => this.age;
+  public setAge = (age: number) => this.age = age
+
+  public getLang = () => this.lang;
+
+  public setLang(lang: string) {
+    this.lang = lang;
+  }
+
+  public greet() {
+    console.log(this.getGreetMsg(this.name));
+  }
+
+  private getGreetMsg(name: string) {
+    return `Hello, my name is ${name}`
+  }
+}
+
+const bob = new Person("Bob", "jazz", 16, "Australian english");
+bob.getAge()
