209cf8057be5854dc55db72c7e3bb0fda8683fd3
Matvey <morozovmatvej75@gmail.com>
2023-02-20 17:39:20 +0300

    generics

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-function makeArr<T>(x: T) {
-  return [x];
+function makeTuple<T, Y>(x: T, y: Y): [T, Y] {
+  return [x, y];
 }
 
-function makeTuple<T>(x: T): [T] {
-  return [x];
+function makeArr<T>(...x: T[]): T[] {
+  return x;
 }
 
-const v1 = makeArr(2);
+const arr = makeArr("t", "t", "t");
+const arr2 = makeArr(1, 2, 3, 4);
+console.log(arr);
+console.log(arr2);
+
+const tuple1 = makeTuple("3", 2);
+const tuple2 = makeTuple(true, {});
+console.log(tuple1);
+console.log(tuple2);
+// tuple1[2] = 22; // won't work
