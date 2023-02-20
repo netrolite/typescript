99fedb73b76e667c06d81b969132044ac963ccc0
Matvey <morozovmatvej75@gmail.com>
2023-02-20 17:02:42 +0300

    generics basics

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-type IncomeStreams = "salary" | "business" | "bonus" | "robbery";
-type NumOrStr = number | string;
-type Income = Record<IncomeStreams, NumOrStr>;
-
-const income: Income = {
-  robbery: 100,
-  salary: 200,
-  business: 600,
-  bonus: "negative fifty"
+function makeArr<T>(x: T) {
+  return [x];
 }
+
+function makeTuple<T>(x: T): [T] {
+  return [x];
+}
+
+const v1 = makeArr(2);
