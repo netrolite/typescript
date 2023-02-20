46d45023b3f5a4146775ae818c92e1b6a84a7a23
Matvey <morozovmatvej75@gmail.com>
2023-02-20 10:04:29 +0300

    Person class and Developer subclass

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,59 @@
-class Invoice {
-  constructor(
-    public client: string,
-    public details: string,
-    public amount: number
-  ) {}
+class Person {
+  private static _count = 0;
 
-  format() {
-    return `${this.client} is charged $${this.amount} for ${this.details}`
+  constructor(
+    private _name: string,
+    private _age: number,
+    private _address: string,
+    private _dateOfBirth: Date
+  ) {
+    Person.incrementCount();
+  }
+
+  static get count() { return Person._count }
+  static incrementCount() { Person._count += 1 }
+
+  get name() { return this._name }
+  set name(name: string) {
+    this._name = name;
+  }
+
+  get age() { return this._age }
+  set age(age: number) {
+    this._age = age;
+  }
+
+  get address() { return this._address }
+  set address(address: string) {
+    this._address = address;
+  }
+
+  get dateOfBirth() { return this._dateOfBirth }
+  set dateOfBirth(dateOfBirth: Date) {
+    this.dateOfBirth = dateOfBirth;
   }
 }
 
-const invoices: Invoice[] = [];
+class Developer extends Person {
+  constructor(
+    _name: string,
+    _age: number,
+    _address: string,
+    _dateOfBirth: Date,
+    private _languages: string[] = []
+  ) {
+    super(_name, _age, _address, _dateOfBirth);
+  }
 
-invoices.push(new Invoice("Bob", "washing", 120));
-invoices.push(new Invoice("Rob", "writing code", 400));
-console.log(invoices);
-
-for (const invoice of invoices) {
-  console.log(invoice.format());
+  get languages() { return this._languages }
+  set languages(languages: string[]) {
+    this._languages = languages;
+  }
 }
+
+const dev1 = new Developer("bob", 18, "main st", new Date());
+const dev2 = new Developer("bob", 18, "main st", new Date());
+const dev3 = new Developer("bob", 18, "main st", new Date());
+console.log(dev1);
+console.log(dev1.address);
+console.log(Person.count);
