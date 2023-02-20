704beff4fc254931dec94f166d26f0a52217a672
Matvey <morozovmatvej75@gmail.com>
2023-02-20 18:34:14 +0300

    add comment

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// addId only accepts an object as its only argument and captures what that object is using T
 function addId<T extends object>(obj: T) {
   const id = Math.floor(Math.random() * 1000);
   return { ...obj, id };
