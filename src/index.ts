6fad91db61ed325830e2736e2fca4eaed11fa0ff
Matvey <morozovmatvej75@gmail.com>
2023-02-20 15:51:49 +0300

    Record utility type

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,12 @@
-interface Student {
-  name: string,
-  gpa: number,
-  classes?: number[]
-}
+type IncomeStreams = "salary" | "bonus" | "business" | "side hustle";
 
-const student: Student = {
-  name: "douglas",
-  gpa: 3.3,
-  classes: [100, 300]
-}
+// error: index signature parameter type cannot be a literal type or generic type
+/*
+  type Income = {
+    [key: IncomeStreams]: number
+  }
+*/
 
-for (const key in student) {
-  // "keyof Student" works if we have access to Student interface
-  console.log(`${key}: ${student[key as keyof Student]}`);
-}
-
-Object.keys(student).forEach((key) => {
-  // "keyof typeof student" works if we don't have access to Student interface
-  console.log(`${key}: ${student[key as keyof typeof student]}`);
-})
-
-// "keyof Interface" returns all types of values that the interface contains
-// function test(key: keyof Student) === function test(key: string | number | number[] | undefined) 
-function logStudentKey(student: Student, key: keyof Student) {
-  console.log(student[key]);
-}
-
-logStudentKey(student, "classes");
+// no errors
+// creates Income type that can have IncomeStreams as keys and numbers as values
+type Income = Record<IncomeStreams, number>;
