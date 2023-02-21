2b21e6cc9af287a31af5a9a4c3a2e54969e4c581
Matvey <morozovmatvej75@gmail.com>
2023-02-21 18:12:34 +0300

    generics extends

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-const CURRENCIES = new Map();
-const us = { name: "United States of America" };
-const ru = { name: "Russian Federation" };
-const ind = { name: "India" };
-CURRENCIES.set(us, "USD");
-CURRENCIES.set(ru, "Ruble");
-CURRENCIES.set(ind, "Rupee");
+interface Person {
+  firstName: string,
+  lastName: string
+}
 
-CURRENCIES.forEach((val, key) => {
-  key = JSON.stringify(key);
-  console.log(`${key}: ${val}`)
-});
+// generics allow for adding properties not specified in "Person" interface (the ones specified are still required)
+// e.g it's possible to add property "age" to "person" argument
+function addFullName<T extends Person>(person: T) {
+  const { firstName, lastName } = person;
+  const fullName = makeFullName(firstName, lastName);
+  return { ...person, fullName };
+}
+
+const person = addFullName({ firstName: "bob", lastName: "banks", age: "15" });
+console.log(person);
+
+function makeFullName(firstName: string, lastName: string) {
+  firstName = firstName.trim();
+  lastName = lastName.trim();
+  return `${firstName} ${lastName}`;
+}
