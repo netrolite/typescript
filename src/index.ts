d4989f482c1a575063af5c7f197a4f8b19567667
Matvey <morozovmatvej75@gmail.com>
2023-02-21 19:02:52 +0300

    generics for interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
-interface Person {
-  firstName: string,
-  lastName: string
+interface Tab<T> {
+  id: string,
+  position: number,
+  data: T
 }
 
-// generics allow for adding properties not specified in "Person" interface (the ones specified are still required)
-// e.g it's possible to add property "age" to "person" argument
-function addFullName<T extends Person>(person: T) {
-  const { firstName, lastName } = person;
-  const fullName = makeFullName(firstName, lastName);
-  return { ...person, fullName };
-}
-
-const person = addFullName({ firstName: "bob", lastName: "banks", age: "15" });
-console.log(person);
-
-function makeFullName(firstName: string, lastName: string) {
-  firstName = firstName.trim();
-  lastName = lastName.trim();
-  return `${firstName} ${lastName}`;
-}
+type NumberTab = Tab<number>;
+type stringTab = Tab<string>;
+type booleanTab = Tab<boolean>;
+type objectTab = Tab<object>;
+type numberArrTab = Tab<Array<number>>;
+type stringArrTab = Tab<Array<string>>;
+type booleanArrTab = Tab<Array<boolean>>;
