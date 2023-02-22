e0b421b2857cf87fba538488ae2eed6ae7b46a6b
Matvey <morozovmatvej75@gmail.com>
2023-02-22 14:39:35 +0300

    unknown type another example

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-let x: any;
-let y: unknown;
-
-// no errrors, typescirpt not checking anything
-// x.trim();
-
-// error: Object is of type "unknown".
-// y.trim();
-
-// have to check the type before using any type-specific methods like .trim()
-if (typeof y === "string") {
-  console.log(y.trim());
-} else if (typeof y === "number") {
-  console.log(y.toFixed(2));
+function log(val: unknown) {
+  if (typeof val === "number") {
+    console.log(val.toFixed(2));
+  } else if (typeof val === "string") {
+    if (isUpperCase(val)) console.log(val.toLowerCase());
+    else console.log(val.toUpperCase())
+  } else {
+    console.log(val);
+  }
 }
+
+function isUpperCase(s: string) {
+  return s === s.toUpperCase();
+}
+
+log("HELLO");
