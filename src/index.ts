c0ba124853b9ac97bbe647fe1770e808569200fe
Matvey <morozovmatvej75@gmail.com>
2023-02-23 15:06:53 +0300

    combining Record and Partial

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
-// Record utility type
+type Grade = "A" | "B" | "C" | "D" | "F";
+type Students = "John" | "Joe" | "Bob" | "Rob";
+type FinalGrades = Record<Students, Grade>;
 
-// Syntax: Record<keyType: valueType>
-
-const hexColorMap: Record<string, string> = {
-  red: "FF0000",
-  green: "00FF00",
-  blue: "0000FF"
+const arr: number[] = [];
+const finalGrades: Partial<FinalGrades> = {
+  Rob: "B"  
 }
 
-const incomeStreams: Record<string, number> = {
-  salary: 10000,
-  freelance: 8000,
-  "side hustle": 6000
-}
-
-const things: Record<number, unknown> = {
-  0: true,
-  1: "hello",
-  2: ["this", "is", "my", "real", "array"],
-  3: { this: "is", my: "object" }
-}
