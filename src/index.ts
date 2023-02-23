7ed0f9c66b2e85ab873236581235b0031d7080ab
Matvey <morozovmatvej75@gmail.com>
2023-02-23 21:47:29 +0300

    Exclude<T, K> and Extract<T, K>

--- src/index.ts
+++ src/index.ts
@@ -12,15 +12,6 @@ type AssignmentPreview = Omit<Assignment, "maxScore" | "passingScore">;
 // type AssignmentResults = Pick<AssignmentAllPropsRequired, "title" | "passingScore" | "score">;
 type AssignmentResults = Required<Pick<Assignment, "title" | "passingScore" | "score">>;
 
-const asgmt1: Assignment = {
-  title: "write an article on child labor",
-  studentId: 120,
-  maxScore: 10,
-  passingScore: 3  
-}
-
-const results: AssignmentResults = {
-  title: "Your article on child labor",
-  passingScore: 3,
-  score: 2
-}
+type Grade = "A" | "B" | "C" | "D" | "F";
+type UnsatisfactoryGrade = Extract<Grade, "F">;
+type SatisfactoryGrade = Exclude<Grade, "F">;
