17c42f2b9f6a14e510863e73df279b3f231f7feb
Matvey <morozovmatvej75@gmail.com>
2023-02-23 11:03:43 +0300

    extends

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,23 @@
-function isObject(arg: unknown) {
-  return (
-    typeof arg === "object" &&
-    !Array.isArray(arg) &&
-    arg !== null
-  )
+interface HasId {
+  id: number
 }
 
-interface BoolCheck<T> {
-  data: T,
-  is: boolean
+// can have properties other than id
+function processUser<T extends HasId>(user: T) {
+  return user;
 }
 
-// "<T extends Record<string, unknown>" only allows to pass "arg" as an object with its keys being of type string
-// On the other hand, using "<T extends {}>" allows to pass argument of any type, such as string or a boolean
-// that is because in TypeScript, primitive types are derivatives of object type 
-function isTrue<T extends Record<string, unknown>>(arg: T): { data: T, is: boolean } {
-  if (isObject(arg)) {
-    const length = Object.keys(arg).length;
-    return { data: arg, is: !!length };
-  }
-  if (Array.isArray(arg)) {
-    return { data: arg, is: !!arg.length };
-  }
-  return { data: arg, is: !!arg };
+// can only have id property
+function processId(id: HasId) {
+  return id;
 }
 
 
-interface Animal {
-  name: string,
-  age: number
-}
+// ok
+console.log(processUser({ id: 10, name: "matvey" }));
 
-interface Cat extends Animal {
-  color: string
-}
+// error
+// console.log(processId({ id: 10, name: "matvey" }));
 
-function logAnimalProp(prop: keyof Animal) {
-  console.log(prop);
-}
-
-const cat: Cat = {
-  color: "brown",
-  name: "Bowser",
-  age: 1
-}
-
-// if you only pass "cat.name", TypeScript is not going to be able to infer that it's a key of type Animal. This is because the compiler only looks at the type of "cat.name", which is a string.
-logAnimalProp(cat.name as keyof Animal);
-
+// ok
+console.log(processId({ id: 12 }));
