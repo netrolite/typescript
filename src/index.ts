a5ea51f653c54a68d503af3ca960c3f60f4ad32a
Matvey <morozovmatvej75@gmail.com>
2023-02-23 15:47:17 +0300

    write my implementation of Pick<T, K> and use it

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
 type Grade = "A" | "B" | "C" | "D" | "F";
 type Students = "John" | "Joe" | "Bob" | "Rob";
-type FinalGrades = Record<Students, Grade>;
+type FinalGrades = Partial<Record<Students, Grade>>;
 
-const arr: number[] = [];
-const finalGrades: Partial<FinalGrades> = {
-  Rob: "B"  
+const finalGrades: FinalGrades = {
+  Joe: "D"
 }
 
+// Pick<T> and Omit<T>
+interface Assignment {
+  title: string,
+  description: string,
+  studentId: number,
+  maxScore: number,
+  passingScore: number
+}
+
+const ass: Assignment = {
+  title: "do this",
+  description: "sample",
+  studentId: 10,
+  maxScore: 100,
+  passingScore: 30
+}
+
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
+type AssignmentResult = MyPick<Assignment, "title" | "passingScore">;
+
+const assResult: AssignmentResult = {
+  title: "This is the title of the assignment",
+  passingScore: 25
+}
