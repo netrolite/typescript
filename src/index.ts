b1b2f148e6a87e3e167a231d284f9489f23bec21
Matvey <morozovmatvej75@gmail.com>
2023-02-26 11:40:07 +0300

    figuring out what async fns return

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,46 @@ function sum(...nums: number[]) { // rest operator
   return nums.reduce((acc, curr) => acc + curr, 0);
 }
 
-console.log(sum(1,2,3,4,5,6));
+
+// Awaited - helps with return type of a Promise
+interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string
+}
+
+async function fetchUsers(): Promise<User[]> {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  // All async fns implicitly return a promise. And if the fn explicitly returns any promises inside of it, they're automatically wrapped inside the outer promise. In this case, the Promise returned by response.json() is automatically wrapped inside the Promise implicitly returned by fetchUsers(). Therefore, when you await the Promise returned by fetchUsers() in the try-catch block, it will wait for both fetch() and response.json() to complete before resolving the Promise and returning the parsed JSON data. However, it is still recommended to await the json() method to avoid any unexpected behavior that may occur due to unhandled Promise rejections.
+  // return response.json();
+  return await response.json();
+}
+
+async function fetchData() {
+  // setTimeout is async, so the function just skips it and returns undefined
+  // setTimeout(() => {
+  //   return "some data";
+  // }, 100)
+
+  // the proper approach is to explicitly return a promise that resolves after 100ms (even though setTimeout is async)
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("some data");
+    }, 100)
+  })
+}
+
+(async () => {
+  const response = fetchData();
+  console.log(response);
+})();
+
+(async () => {
+  try {
+    const users = await fetchUsers();
+    console.log(users);
+  } catch (err) {
+    console.error(err);
+  }
+})();
