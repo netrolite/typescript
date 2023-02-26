d6cbb41808c8e1ae47c108743f829bf0dc3ac3ab
Matvey <morozovmatvej75@gmail.com>
2023-02-26 10:19:54 +0300

    rest vs spread operator

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,13 @@ function createCar(horsepower: number, isConvertible: boolean, make: string) {
 
 type Car = ReturnType<typeof createCar>;
 type CarParams = Parameters<typeof createCar>;
+
+const args: CarParams = [120, false, "GM"];
+// const badArgs = [120, false, "GM"] // won't work. Only tuples can be used with the spread operator
+const car: Car = createCar(...args); // spread operator
+
+function sum(...nums: number[]) { // rest operator
+  return nums.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(sum(1,2,3,4,5,6));
