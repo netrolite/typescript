9357fdab6e495727579918e10ceddf01669606f6
Matvey <morozovmatvej75@gmail.com>
2023-02-26 11:57:52 +0300

    Awaited<ReturnType<typeof T>>

--- src/index.ts
+++ src/index.ts
@@ -24,35 +24,14 @@ interface User {
 
 async function fetchUsers(): Promise<User[]> {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  // All async fns implicitly return a promise. And if the fn explicitly returns any promises inside of it, they're automatically wrapped inside the outer promise. In this case, the Promise returned by response.json() is automatically wrapped inside the Promise implicitly returned by fetchUsers(). Therefore, when you await the Promise returned by fetchUsers() in the try-catch block, it will wait for both fetch() and response.json() to complete before resolving the Promise and returning the parsed JSON data. However, it is still recommended to await the json() method to avoid any unexpected behavior that may occur due to unhandled Promise rejections.
-  // return response.json();
   return await response.json();
 }
 
-async function fetchData() {
-  // setTimeout is async, so the function just skips it and returns undefined
-  // setTimeout(() => {
-  //   return "some data";
-  // }, 100)
-
-  // the proper approach is to explicitly return a promise that resolves after 100ms (even though setTimeout is async)
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("some data");
-    }, 100)
-  })
-}
-
 (async () => {
-  const response = fetchData();
-  console.log(response);
+  const users = await fetchUsers();
+  console.log(users);
 })();
 
-(async () => {
-  try {
-    const users = await fetchUsers();
-    console.log(users);
-  } catch (err) {
-    console.error(err);
-  }
-})();
+
+// Awaited<typeof fetchUsers>; would return Promise<Users[]> because typeof fetchUsers gives you the entire function including its arguments like `(id: number) => Promise<Student>`. And Awaited<T> converts this to just the return type, which is `Promise<Student>`. To avoid that, you should use Awaited<ReturnType<typeof test>> to make sure that you're passing only the return type of `test` (which is Promise<Student>) to Awaited<T>, which would give you type `Student`.
+type Test = Awaited<ReturnType<typeof fetchUsers>>;
