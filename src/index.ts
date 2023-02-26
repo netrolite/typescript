7bcba215f1423f217cfe7337913397e556030280
Matvey <morozovmatvej75@gmail.com>
2023-02-26 09:48:35 +0300

    derive type from function return type

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,5 @@
-interface Assignment {
-  title: string,
-  details?: string,
-  studentId: number,
-  maxScore: number,
-  passingScore: number,
-  score?: number
+type Student = ReturnType<typeof createStudent>;
+function createStudent(name: string, age: number, email: string) {
+  return { name, age, email };
 }
-
-type AssignmentAllPropsRequired = Required<Assignment>;
-type AssignmentPreview = Omit<Assignment, "maxScore" | "passingScore">;
-// type AssignmentResults = Pick<AssignmentAllPropsRequired, "title" | "passingScore" | "score">;
-type AssignmentResults = Required<Pick<Assignment, "title" | "passingScore" | "score">>;
-
-type Grade = "A" | "B" | "C" | "D" | "F";
-type UnsatisfactoryGrade = Extract<Grade, "F">;
-type SatisfactoryGrade = Exclude<Grade, "F">;
-type HighGrade = Extract<Grade, "A" | "B">;
-
-type Student = "John" | null | "Dave" | undefined;
-type StudentNoNullOrUndefiend = NonNullable<Student>;
+console.log(typeof createStudent);
